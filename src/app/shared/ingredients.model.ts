c5c26e7de44211bb4c2144f0d3f185aea26881d4
jitiendran <jitiendranjiji2000@gmail.com>
2021-01-22 14:09:49 +0530

    Added shopping list

---
+++ src/app/shared/ingredients.model.ts
@@ -0,0 +1,8 @@
+export class Ingredient{
+    public Name:string
+    public Amount:number
+    constructor(Name:string,Amount:number){
+        this.Name = Name
+        this.Amount = Amount
+    }
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
+import { Ingredient } from './../shared/ingredients.model';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
-
+  Ingredients:Ingredient[] = [
+    new Ingredient('Tomato',5),
+    new Ingredient('Onion',2)
+  ]
   constructor() { }
 
   ngOnInit(): void {
